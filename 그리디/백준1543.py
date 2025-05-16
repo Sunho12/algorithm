bf215b65b7b1106827b017cf05c3d7fab8390bf5
Sunho12 <rhkrtjsgh1210@gmail.com>
2025-05-16 22:03:36 +0900

    upload files

---
+++ 그리디/백준1543.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+s = input().rstrip()
+search = input().rstrip()
+cnt = 0
+length = len(search)
+i = 0
+
+while i < (len(s) - length):
+    print(s[i:i+length])
+    if s[i:i+length] == search:
+        cnt += 1
+        i += length
+    else:
+        i += 1
+
+print(cnt)
